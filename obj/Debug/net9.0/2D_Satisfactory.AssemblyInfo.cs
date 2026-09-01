92250f287dc984438b7a2a4ae955aea1d16d831b
daganspano <daganspano@gmail.com>
2026-08-31 21:31:11 -0500

    TestSync Error

--- obj/Debug/net9.0/2D_Satisfactory.AssemblyInfo.cs
+++ obj/Debug/net9.0/2D_Satisfactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2D_Satisfactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06948b52ca4d947fd228c1f65d1ee96ae7ecd51d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f4877094c018c9cc95dbf0b7226a800047929de")]
 [assembly: System.Reflection.AssemblyProductAttribute("2D_Satisfactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2D_Satisfactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
